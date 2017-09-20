af2e636df9d2ce07d5b0a062fbcebe985b6f772c
Robert Davis <rdavis4677@gmail.com>
2017-09-19 21:01:09 -0400

    added some comments

--- GPU_set_clocks.ps1
+++ GPU_set_clocks.ps1
@@ -1,3 +1,4 @@
+#always run with admin privileges
 param([switch]$Elevated)
 function Check-Admin {
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -14,16 +15,17 @@ if ((Check-Admin) -eq $false)  {
 exit
 }
 $error.clear()
+#main code
 try{
     cd "C:\Program Files\NVIDIA Corporation\NVSMI\"
     [int]$numGPU = 0
     $data = (./nvidia-smi -q -d SUPPORTED_CLOCKS)
-    [regex]$regex = "Supported Clocks\s+Memory\ +:\ +(?<mem>\d+)+ MHz\s+Graphics\ +:\ (?<core>\d+)\ MHz"
-    $regex.Matches($data)| %{ ./nvidia-smi -ac $_.Groups['mem'].value,$_.Groups['core'].value -i ($numGPU++)    }
+    [regex]$regex = "Supported Clocks\s+Memory\ +:\ +(?<mem>\d+)+ MHz\s+Graphics\ +:\ (?<core>\d+)\ MHz" #regex to parse out mem and core clock speed
+    $regex.Matches($data)| %{ ./nvidia-smi -ac $_.Groups['mem'].value,$_.Groups['core'].value -i ($numGPU++)    } #iterate through all GPUs and set them to highest clock
 }
-catch{ "Error Occured" }
+catch{ "Error Occured" } #lazy catch statement
 if(!$error){
-Start-Sleep -s 2
+Start-Sleep -s 2 #display success for 2 seconds before closing
 exit
 }
 
