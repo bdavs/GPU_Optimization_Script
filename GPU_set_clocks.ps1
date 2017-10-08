e8165bcc07401fe96b141eb4e9c3e154ca26f14b
Daniel Garcia <dgarcia1@umassd.edu>
2017-10-08 15:20:23 -0400

    Made debugging a bit easier

--- GPU_set_clocks.ps1
+++ GPU_set_clocks.ps1
@@ -1,18 +1,18 @@
 #always run with admin privileges
 param([switch]$Elevated)
 function Check-Admin {
-$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 if ((Check-Admin) -eq $false)  {
     if ($elevated)
     {
     # could not elevate, quit
-    exit
+        exit
     } else {
-    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile  -executionpolicy bypass -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile  -executionpolicy bypass -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
-exit
+    exit
 }
 $error.clear()
 #main code
@@ -23,7 +23,7 @@ try{
     [regex]$regex = "Supported Clocks\s+Memory\ +:\ +(?<mem>\d+)+ MHz\s+Graphics\ +:\ (?<core>\d+)\ MHz" #regex to parse out mem and core clock speed
     $regex.Matches($data)| %{ ./nvidia-smi -ac $_.Groups['mem'].value,$_.Groups['core'].value -i ($numGPU++)    } #iterate through all GPUs and set them to highest clock
 }
-catch{ "Error Occured" } #lazy catch statement
+catch{ "Error Occured: $_" } #not so lazy catch statement
 if(!$error){
 Start-Sleep -s 2 #display success for 2 seconds before closing
 exit
